3ab2e39529b3f667d1c6675e8168dac2dcdd6c70
Daniel PC <daniel.barczynski@onet.pl>
2022-10-08 20:13:47 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,41 +1,43 @@
-﻿Console.WriteLine("Podaj liczbe: ");
+﻿Console.Write("Podaj liczbe: ");
 string number = Console.ReadLine();
 int n = Convert.ToInt32(number);
 // int n = 33;
 List<int> numbers = new List<int>();
 
-Console.WriteLine("Liczby pierwsze: ");
+Console.WriteLine();
+Console.WriteLine("Liczby pierwsze:\n ");
 
 for (int i = 2; i <= n; i++)
 {
     if (i == 2 || i == 3 || i == 5)
     {
-        Console.WriteLine(i);
+        Console.Write($"{i} ");
         numbers.Add(i);
     }
     else if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0)
     {
-        Console.WriteLine(i);
+        Console.Write($"{i} ");
         numbers.Add(i);
     }
 }
 
-Console.WriteLine("Liczby bliźniacze: ");
+Console.WriteLine();
+Console.WriteLine("\nLiczby bliźniacze:\n ");
 
 int lastNum = numbers.Last();
 int skip = 0;
-int increment = 1;
 
 foreach (var item in numbers)
 {
-    skip += increment;
+    skip++;
     var nextItem = numbers.Skip(skip).FirstOrDefault();
     if(nextItem >= lastNum)
         break;
     else if (nextItem - item == 2)
     {
-        Console.WriteLine($"{item}, {nextItem}");
+        Console.Write($"[{item}, {nextItem}] ");
     }
 }   
 
-Console.WriteLine("Liczby czworacze: ");
+Console.WriteLine();
+Console.WriteLine("\nLiczby czworacze:\n ");
